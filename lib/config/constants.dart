2bcceb7010d52d22534a2cb2a858f2d58c092395
Rudy Ailabouni <rudy.ailabouni@inovex.de>
2022-12-27 10:47:29 +0100

    added light and dark themes

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,3 +1,4 @@
 abstract class Constants {
-  static const inoventory_backend_url = "http://10.100.255.76:8080";
+  static const inoventoryBackendUrl = "http://10.100.255.76:8080";
+  static const darkMode = true;
 }
--- lib/views/InoventoryApp.dart
+++ lib/views/InoventoryApp.dart
@@ -1,31 +1,21 @@
 
 import 'package:flutter/material.dart';
+import 'package:inoventory_ui/config/constants.dart';
+import 'package:inoventory_ui/config/themes.dart';
 import 'package:inoventory_ui/views/home_view.dart';
 
 class InoventoryApp extends StatelessWidget {
   final String title;
-  final themeDataLight = ThemeData(
-    primaryColor: const Color(0x0000BCD4),
-    colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.cyan).copyWith(secondary: Colors.orange),
-    textTheme: const TextTheme(
-      headline1: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold, color: Colors.black),
-      headline2: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.black),
-      headline3: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.black),
-      headline4: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.black),
-      bodyText1: TextStyle(fontSize: 16.0, color: Colors.black),
-      bodyText2: TextStyle(fontSize: 14.0, color: Colors.black),
-    ),
-  );
 
 
-  InoventoryApp({super.key, this.title = "inoventory"});
+  const InoventoryApp({super.key, this.title = "inoventory"});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: title,
-      theme: ThemeData.dark(), // themeDataLight,
+      theme: Constants.darkMode ? darkTheme : lightTheme, // themeDataLight,
       home: const InoventoryHome()
     );
   }
