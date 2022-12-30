30f877fd941d5fef42c7b86c3a1aa0f74463bbdf
Rudy Ailabouni <rudy.ailabouni@inovex.de>
2022-12-30 14:38:55 +0100

    update backend URL to dev server

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,5 +1,5 @@
 abstract class Constants {
-  // static const inoventoryBackendUrl = "http://10.100.255.76:8080";
-  static const inoventoryBackendUrl = "http://10.0.2.2:8080";
+  static const inoventoryBackendUrl = "http://10.100.255.76:8080";
+  // static const inoventoryBackendUrl = "http://10.0.2.2:8080";
   static const darkMode = true;
 }
