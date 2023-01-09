03302a015defa70ac5a1125c1c16d6c8c570ab7e
Sören Uebis <soeren.uebis@inovex.de>
2023-01-09 14:23:18 +0100

    refactor inventory item widget stateless

--- lib/inventory/items/inventory_item_widget.dart
+++ lib/inventory/items/inventory_item_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'inventory_item.dart';
 import 'dart:developer' as developer;
 
-class InventoryItemWidget extends StatefulWidget {
+class InventoryItemWidget extends StatelessWidget {
   final InventoryListItemWrapper itemWrapper;
   final Future<void> Function(int listId, int itemId) onDelete;
 
@@ -12,43 +12,33 @@ class InventoryItemWidget extends StatefulWidget {
     Key? key,
   }) : super(key: key);
 
-  @override
-  State<InventoryItemWidget> createState() => _InventoryItemWidgetState();
-}
+  Future<void> onDismissed(
+      BuildContext context, DismissDirection direction) async {
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
 
-class _InventoryItemWidgetState extends State<InventoryItemWidget> {
-  bool isDeleting = false;
+    try {
+      await onDelete(itemWrapper.listId, itemWrapper.items.first.id);
 
-  Future<void> onDismissed(DismissDirection direction) async {
-    if (!isDeleting) {
-      print("Deleting item");
+      scaffoldMessenger.showSnackBar(
+          _getSnackBar("Successfully deleted item", Colors.green));
+    } catch (e) {
+      scaffoldMessenger
+          .showSnackBar(_getSnackBar("Error deleting item: ", Colors.red));
 
-      final scaffoldMessenger = ScaffoldMessenger.of(context);
-      try {
-        isDeleting = true;
-        await widget.onDelete(
-            widget.itemWrapper.listId, widget.itemWrapper.items.first.id);
-        scaffoldMessenger.showSnackBar(const SnackBar(
-            content: Text("Successfully deleted item!",
-                style: TextStyle(color: Colors.white)),
-            backgroundColor: Colors.green));
-      } catch (e) {
-        scaffoldMessenger.showSnackBar(SnackBar(
-            content: Text("Error deleting item: ${e.toString()}",
-                style: const TextStyle(color: Colors.white)),
-            backgroundColor: Colors.red));
-        
-        developer.log("Could not delete item", error: e);
-      } finally {
-        isDeleting = false;
-      }
+      developer.log("Could not delete item", error: e);
     }
+  }
+
+  SnackBar _getSnackBar(String text, Color color) {
+    TextStyle style = const TextStyle(color: Colors.white);
+
+    return SnackBar(content: Text(text, style: style), backgroundColor: color);
   }
 
   @override
   Widget build(BuildContext context) {
     return Dismissible(
-      key: Key(widget.itemWrapper.productEan),
+      key: Key(itemWrapper.productEan),
       direction: DismissDirection.endToStart,
       background: Container(
         color: Colors.red,
@@ -56,35 +46,44 @@ class _InventoryItemWidgetState extends State<InventoryItemWidget> {
         child: const Icon(Icons.delete, color: Colors.white),
       ),
       onDismissed: (direction) async {
-        await onDismissed(direction);
+        await onDismissed(context, direction);
       },
       resizeDuration: null,
       child: ListTile(
-        // leading: icon
-        title: Text(
-          widget.itemWrapper.displayName ?? widget.itemWrapper.productEan,
+          // leading: icon
+          title: Text(
+            itemWrapper.displayName ?? itemWrapper.productEan,
+            style: const TextStyle(fontSize: 18),
+          ),
+          subtitle: Text(
+            itemWrapper.productEan,
+            style: const TextStyle(fontSize: 14, color: Colors.grey),
+          ),
+          trailing: itemWrapper.quantity > 1
+              ? _QuantityWidget(itemWrapper.quantity)
+              : null),
+    );
+  }
+}
+
+class _QuantityWidget extends StatelessWidget {
+  const _QuantityWidget(this._quantity, {Key? key}) : super(key: key);
+
+  final int _quantity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondary,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        child: Text(
+          "Quantity: $_quantity",
           style: const TextStyle(fontSize: 18),
         ),
-        subtitle: Text(
-          widget.itemWrapper.productEan,
-          style: const TextStyle(fontSize: 14, color: Colors.grey),
-        ),
-        trailing: widget.itemWrapper.quantity > 1
-            ? Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.secondary,
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                  child: Text(
-                    "Quantity: ${widget.itemWrapper.quantity.toString()}",
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                ),
-              )
-            : null,
       ),
     );
   }
