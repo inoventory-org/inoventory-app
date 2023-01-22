6065619882cd7612b6443ddb8508e1099e415ea4
Rudy Ailabouni <rudy.ailabouni@inovex.de>
2023-01-22 14:48:24 +0100

    point app to new public backend

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
 
+
+abstract class Constants {
+  // static const inoventoryBackendUrl = "http://10.100.255.76:8080";
+  // static const inoventoryBackendUrl = "http://localhost:8080";
+  // static const inoventoryBackendUrl = "http://10.0.2.2:8080";
+  static const inoventoryBackendUrl = "https://www.inoventory.railabouni.fra.ics.inovex.io";
+  static const darkMode = true;
+  static final keycloakConf = KeycloakConf();
+  static const accessTokenRefreshIntervalSeconds = 60;
+}
+
+
+
 class KeycloakConf {
-  static const baseUrl = "http://10.100.255.76:8081";
+  static const baseUrl = "${Constants.inoventoryBackendUrl}/auth";
   static const realm = "inoventory";
   static const baseUrlWithRealm = "$baseUrl/realms/$realm";
   final authorizationUrl = "$baseUrlWithRealm/protocol/openid-connect/auth";
@@ -10,15 +23,5 @@ class KeycloakConf {
   final clientId = "app";
   static final redirectSchema = "com.railabouni.inoventory";
   final redirectUrl =
-      kIsWeb ? "http://localhost:50000/auth" : "$redirectSchema://auth";
-}
-
-abstract class Constants {
-  static const inoventoryBackendUrl = "http://10.100.255.76:8080";
-  // static const inoventoryBackendUrl = "http://localhost:8080";
-  // static const inoventoryBackendUrl = "http://10.0.2.2:8080";
-  // static const inoventoryBackendUrl = "http://ec2-18-157-81-199.eu-central-1.compute.amazonaws.com";
-  static const darkMode = true;
-  static final keycloakConf = KeycloakConf();
-  static const accessTokenRefreshIntervalSeconds = 60;
+  kIsWeb ? "http://localhost:50000/auth" : "$redirectSchema://auth";
 }
