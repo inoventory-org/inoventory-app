d804cc11633afc09f2b13c839ba2c1d559d7848f
Rudy Ailabouni <eilabouni.rudy@gmail.com>
2026-03-25 20:33:09 +0100

    move to render backend

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,10 +1,12 @@
 abstract class Constants {
   // static const inoventoryBackendUrl = "http://10.100.255.76:8080";
-  static const inoventoryBackendUrl = "http://localhost:8080";
+  // static const inoventoryBackendUrl = "http://localhost:8080";
   // static const inoventoryBackendUrl = "http://10.0.2.2:8080";
   static const appName = "inoventory";
   static const version = "0.0.1";
   // static const inoventoryBackendUrl = "https://www.inoventory.railabouni.fra.ics.inovex.io";
+  static const inoventoryBackendUrl = "https://inoventory.onrender.com";
+
   static const darkMode = true;
   static const openFoodFactsUserName = "inoventory";
   static String? supabaseRedirectUrl(bool isWeb) =>
